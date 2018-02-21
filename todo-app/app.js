e17efa046052a0ad1dadcc1f9b5c908ba4057eae
Maira Martins K <mairamartinsk@gmail.com>
2018-02-21 10:12:23 +0100

    Create HTML file and add CSS styling

--- todo-app/app.js
+++ todo-app/app.js
@@ -6,9 +6,11 @@ const todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res) {
-  res.send('Root route');
+  res.sendFile('index.html');
 });
 
 app.use('/api/todos', todoRoutes);
