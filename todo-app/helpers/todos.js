6849f357b12906d9835c38007cc65a5fa06f5234
Maira Martins K <mairamartinsk@gmail.com>
2018-02-21 09:45:31 +0100

    Refactor and move routes to helper file

---
+++ todo-app/helpers/todos.js
@@ -0,0 +1,53 @@
+const db = require('../models');
+
+exports.getTodos = function(req, res) {
+  db.Todo.find()
+    .then(function(todos) {
+      res.json(todos);
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+};
+
+exports.createTodo = function(req, res) {
+  db.Todo.create(req.body)
+    .then(function(newTodo) {
+      res.status(201).json(newTodo);
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+};
+
+exports.showTodo = function(req, res) {
+  db.Todo.findById(req.params.todoId)
+    .then(function(showTodo) {
+      res.json(showTodo);
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+};
+
+exports.updateTodo = function(req, res) {
+  db.Todo.findOneAndUpdate({ _id: req.params.todoId }, req.body, { new: true })
+    .then(function(todo) {
+      res.json(todo);
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+};
+
+exports.deleteTodo = function(req, res) {
+  db.Todo.remove({ _id: req.params.todoId })
+    .then(function() {
+      res.json({ message: 'Todo item deleted.' });
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+};
+
+module.exports = exports;
--- todo-app/routes/todos.js
+++ todo-app/routes/todos.js
@@ -1,55 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const helpers = require('../helpers/todos');
 
-router.get('/', function(req, res) {
-  db.Todo.find()
-    .then(function(todos) {
-      res.json(todos);
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
+router.route('/')
+  .get(helpers.getTodos)
+  .post(helpers.createTodo);
 
-router.post('/', function(req, res) {
-  db.Todo.create(req.body)
-    .then(function(newTodo) {
-      res.status(201).json(newTodo);
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
-
-router.get('/:todoId', function(req, res) {
-  db.Todo.findById(req.params.todoId)
-    .then(function(showTodo) {
-      res.json(showTodo);
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
-
-router.put('/:todoId', function(req, res) {
-  db.Todo.findOneAndUpdate({ _id: req.params.todoId }, req.body, { new: true })
-    .then(function(todo) {
-      res.json(todo);
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
-
-router.delete('/:todoId', function(req, res) {
-  db.Todo.remove({ _id: req.params.todoId })
-    .then(function() {
-      res.json({ message: 'Todo item deleted.' });
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
+router.route('/:todoId')
+  .get(helpers.showTodo)
+  .put(helpers.updateTodo)
+  .delete(helpers.deleteTodo);
 
 module.exports = router;
