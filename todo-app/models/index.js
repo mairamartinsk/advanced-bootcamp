33b584e2343bba06ec267bc433ae2aa79089d4cb
Maira Martins K <mairamartinsk@gmail.com>
2018-02-21 08:35:42 +0100

    Remove useMongoClient (no longer needed in Mongoose v5)

--- todo-app/models/index.js
+++ todo-app/models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/todo-api');
 mongoose.Promise = global.Promise;
 
 module.exports.Todo = require('./todo');
