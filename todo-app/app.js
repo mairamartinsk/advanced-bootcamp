627f0a074e9f6f78669d9681c38502c6bc0add81
Maira Martins K <mairamartinsk@gmail.com>
2018-02-21 09:04:43 +0100

    Require and setup body-parser

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 const todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.send('Root route');
