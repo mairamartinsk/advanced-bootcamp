d8eed113a82ac74532814faba1671b48746332a5
Maira Martins K <mairamartinsk@gmail.com>
2018-02-21 08:38:22 +0100

    Require Todo routes

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const todoRoutes = require('./routes/todos');
 
 app.get('/', function(req, res) {
   res.send('Root route');
 });
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(port, () => {
   console.log('Listening on port ' + port);
